3411c5b1f70ad0f4fd9269460428e2b6c6b9e2ee
msa <msai.sadehi@gmail.com>
2023-06-18 18:01:23 +0330

    first session

--- session1.py
+++ session1.py
@@ -1,2 +1,27 @@
-print("hello world")
-print("Welcome to python class")
+# print("hello world")
+# print("Welcome to python class")
+
+
+# print("score is :", 100)
+
+# string = ""
+# string2 = ''
+
+# print("The file is stored in C:\new folder")
+# print("The file is stored in C:\\new folder")
+
+# x = 0
+# print("x =",x)
+
+# x = x + 1
+# x += 1
+# print("x =",x)
+
+# +     -       *       /
+
+# print(4 % 2)
+# if 4 % 2 == 0:
+#     print("even")
+
+average = (19 + 20 + 18 + 15) / 4
+print(average)
