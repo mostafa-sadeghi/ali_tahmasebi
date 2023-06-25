c1d85ee2853145d47636550f07cbf1d3967295db
msa <msai.sadehi@gmail.com>
2023-06-25 17:55:07 +0330

    sadeghi

---
+++ session2.py
@@ -0,0 +1,100 @@
+# print(2 + 3)
+# print(2 * 3)
+# print(2 / 3)
+# print(2 - 3)
+
+# print(5 / 2)
+# print(5 // 2)
+
+# print(2 + 3 * 2)
+# print((2 + 3) * 2)
+
+# print(4 % 2)
+# print(5 % 2)
+
+
+# name = input("Enter your name:> ")
+# print("your name is:", name)
+
+# number1 = int(input("Enter a number: "))
+# number2 = int(input("enter a number: "))
+# print(number1 + number2)
+
+
+# number1 = float(input("Enter a number: "))
+# number2 = float(input("enter a number: "))
+# print("number1 + number2 =", number1 + number2)
+# print("number1 - number2 =", number1 - number2)
+# TODO تمرین برای ضرب و تقسیم هم انجام بده
+# *   /
+
+# برنامه ای بنویس که نام و نمرات سه درس یک دانش آموز را 
+# از ورودی دریافت نماید و معدل اورا محاسبه کند و نمایش دهد
+# معدل یعنی مجموع نمرات تقسیم بر تعدادشان
+
+
+
+# a = 4
+# b = 3
+
+
+# if a > b:
+#     print("a is greater than b")
+# if a < b:
+#     print("a is less than b")
+#     print("blalall")
+# print("outside")
+
+# a = 4
+# b = 3
+# c = 2
+
+# if a > b and a > c:
+#     print("a is the greatest")
+
+# elif b > a and b > c:
+#     print("b is the greatest")
+
+# elif c > a and c > b:
+#     print("c is the greatest")
+
+
+# user_name = input("Enter the username: ")
+# password = input("Enter the password: ")
+
+# if user_name == "ali" and password == "123":
+#     print("user is valid")
+# else:
+#     print("user is not valid")
+
+# score = 20
+# if score < 20:
+#     print("level one")
+# elif score >= 20:
+#     print("next level")
+
+
+a = 3
+b = 4
+
+if a == b:
+    print("a and b are equal")
+
+if a != b:
+    print("a and b are not equal")
+
+
+# TODO در تمرین قبلی اگر معدل از 
+# نوزده بیشتر بود
+# A را پرینت نماید
+# اگر معدل از هجده بیشتر بود
+# B
+# را پرینت نماید
+# اگر معدل از هفده بیشتر بود
+# C را پرینت نماید
+# اگر معدل از شانزده بیشتر بود
+# D
+# را پرینت نماید
+# درغیر اینصورت 
+# E
+# را پرینت کند
