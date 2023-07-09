647daa1d59afd0c80bbcade632e0b8fdb1517660
msa <msai.sadehi@gmail.com>
2023-07-09 17:59:31 +0330

    sadeghi

---
+++ session4.py
@@ -0,0 +1,66 @@
+# print(22 + 3)
+# print(2 * 4)
+# print(6 / 3)
+# print(2 - 3)
+
+# print(7 / 2)
+# print(7 // 2)
+
+# print(2 + 1 * 2)
+
+# print((2 + 1) * 2)
+
+# print(2 % 2)
+
+# print(3 % 2)
+
+
+# name = input("Enter your name:> ")
+# print("your name is:", name)
+
+# family = input("Enter your family:> ")
+# print("your family is:", family)
+
+
+# age = int(input("Enter your age:> "))
+# print("your age is:", age)
+
+
+# x = int(input("Enter a number: "))
+# y = int(input("Enter a number: "))
+# z = int(input("Enter a number: "))
+# print(x + y + z)
+
+
+# number1 = float(input("Enter first number: "))
+# number2 = float(input("Enter second number: "))
+# print("number1 * number2 =", number1 * number2)
+# print("number1 / number2 =", number1 / number2)
+
+
+# score1 = float(input("Enter first score: "))
+# score2 = float(input("Enter second score: "))
+# score3 = float(input("Enter third score: "))
+
+
+# average = (score1 + score2 + score3)/3
+# print("student's average is:", average)
+
+
+# x = 100
+# if x == 100:
+#     print("ok")
+#     print("A")
+# else:
+#     print("not ok")
+#     print("B")
+
+
+
+x = 200
+if x == 100:
+    print("ok")
+    print("A")
+else:
+    print("not ok")
+    print("B")
