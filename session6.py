1983928ffe161103306503cfa60c3a3057671a08
msa <msai.sadehi@gmail.com>
2023-07-23 17:59:17 +0330

    if statement

---
+++ session6.py
@@ -0,0 +1,50 @@
+# name = input("enter your name: ")
+# score = float(input("enter your programming score: "))
+# if score >= 90:
+#     print(f"{name}, your programming grade is: A")
+
+# elif score >= 80:
+#     print(f"{name}, your programming grade is: B")
+
+# elif score >= 70:
+#     print(f"{name}, your programming grade is: C")
+
+# else:
+#     print(f"{name}, your programming grade is: D")
+
+
+# a = 2
+# b = 3
+# c = 4
+
+
+# if a < b and a < c:
+#     print("a is the smallest")
+
+
+user_name = input("enter the username: ")
+if user_name == "ali" or user_name == "reza":
+    print(f"welcome {user_name}")
+
+else:
+    print(f"{user_name} is not valid")
+
+
+a = True
+if a:
+    print("ok")
+
+# تمرین
+"""
+برنامه ای بنویس که سه عدد از ورودی دریافت نماید 
+و بزرگترین و کوچکترین عدد وارد شده را نمایش دهد
+"""
+
+# تمرین
+"""
+برنامه ای بنویس که دو عدد از ورودی دریافت نماید
+و اگر حاصلجمع دو عدد از 10 بیشتر بودف  پیغام زیر را نمایش دهد
+sum is greater than 10
+اگر حاصلجمع دو عدد از 10 کمتر بود پیغام زیر را نمایش دهد
+sum is less than 10
+"""
